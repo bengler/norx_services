54b5dc33e50250644324805508b8f6be2a10fc0a
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-09-05 22:41:26 +0200

    Fix bootstrap

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Set up services on the Kartverk VM.
 ## Installation
 
 ```cd kartverk_vm_services && ./bootstrap.sh```
+
+If everything works, you should be able to browse the map from:
+
+http://127.0.0.1:3000
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,22 @@
 
 GITHUB_ACCESS_TOKEN="a09e2e7b5488f777a79b82edd506e61ccdfcbe43"
 
-echo  "\t * Setting up TileStache"
-sudo cp ./etc/tilestache /etc/init.d
-sudo chmod 755 /etc/init.d/tilestache
-sudo /etc/init.d/tilestache start
+if [ ! -f '/home/kartverk/done_packages' ]; then
+	echo  "Setting up Tilestache"
+	sudo cp ./etc/tilestache /etc/init.d
+	sudo chmod 755 /etc/init.d/tilestache
+	sudo /etc/init.d/tilestache start
+	touch '/home/kartverk/kartverk_services/tilestache_done'
+fi
 
-echo  "\t * Setting up terrafab"
-git clone "https://$GITHUB_ACCESS_TOKEN@github.com/bengler/terrafab"
-cd terrafab
-npm install
+if [ ! -f '/home/kartverk/done_packages' ]; then
+	echo  "Setting up TerraFab"
+	git clone "https://$GITHUB_ACCESS_TOKEN@github.com/bengler/terrafab"
+	cd terrafab
+	npm install
+	cd ..
+	sudo cp ./etc/terrafab /etc/init.d
+	sudo chmod 755 /etc/init.d/terrafab
+	sudo /etc/init.d/tilestache start
+	touch '/home/kartverk/kartverk_services/tilestache_done'
+fi
