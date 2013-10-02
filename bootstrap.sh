1406bd995b41b4564d7525dee0432b75f33fb02d
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-10-02 23:52:02 +0200

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-name="$1"
+
+name="norx"
 
 if [ ! -f '.done_tilestache' ]; then
 	echo  "Setting up Tilestache"
@@ -12,7 +13,7 @@ fi
 if [ ! -f '.done_leaflet' ]; then
 	echo  "Setting up Leaflet demo app"
   sudo -u $name mkdir leaflet
-	sudo -u $name git clone git://github.com/bengler/$name_leaflet.git leaflet
+	sudo -u $name git clone git://github.com/bengler/norx_leaflet.git leaflet
 	cd leaflet
   npm --silent install
   chown -R $name *
