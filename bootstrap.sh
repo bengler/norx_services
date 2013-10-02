dc0b6fea5e54151074d1c3b6dbbeee3e134b3525
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-10-02 21:45:56 +0200

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,11 +24,11 @@ fi
 if [ ! -f '.done_elasticsearch' ]; then
 	echo "Setting up elastic search postgres bindings"
 	
-  curl -XDELETE 'localhost:9200/_river' && curl -XDELETE 'localhost:9200/places'
+  curl --quiet  -XDELETE 'localhost:9200/_river' && curl --quiet -XDELETE 'localhost:9200/places'
   
-  curl -X PUT localhost:9200/places
+  curl --quiet -X PUT localhost:9200/places
 
-  curl -XPUT localhost:9200/places/place/_mapping -d "{
+  curl --quiet -XPUT localhost:9200/places/place/_mapping -d "{
     \"place\":{
       \"properties\":{
         \"name\" : { \"type\" : \"string\" },
@@ -47,7 +47,7 @@ if [ ! -f '.done_elasticsearch' ]; then
 
 
   SQL="SELECT 'places' as _index, ssr.enh_snavn as \\\"place.name\\\", ssr.ogc_fid as _id, split_part(substring(ST_AsText(ssr.wkb_geometry), '\\\(([^\\\)]+)\\\)'), ' ', 1) as \\\"place.point.lng\\\", split_part(substring(ST_AsText(ssr.wkb_geometry), '\\\(([^\\\)]+)\\\)'), ' ', 2) as \\\"place.point.lat\\\", ssr.enh_navntype as \\\"place.kind\\\", adm_areas_kommuner.navn as \\\"place.parent_name\\\", ssr.enh_snavn || ' ' || adm_areas_kommuner.navn as \\\"suggest.input\\\", ssr.enh_snavn || ' (' || adm_areas_kommuner.navn || ')' as \\\"suggest.output\\\", split_part(substring(ST_AsText(ssr.wkb_geometry), '\\\(([^\\\)]+)\\\)'), ' ', 1) as \\\"suggest.payload.lng\\\", split_part(substring(ST_AsText(ssr.wkb_geometry), '\\\(([^\\\)]+)\\\)'), ' ', 2) as \\\"suggest.payload.lat\\\", ssr.enh_navntype, CASE WHEN ssr.enh_navntype IN(181,182,268,269,270,266) THEN 20 WHEN ssr.enh_navntype IN(101, 104, 108) THEN 10 ELSE 1 END as \\\"suggest.weight\\\" FROM ssr left join adm_areas_kommuner on ST_Within(ssr.wkb_geometry, adm_areas_kommuner.wkb_geometry)"
-  curl -XPUT 'localhost:9200/_river/places_suggest_river/_meta' -d "{
+  curl --quiet -XPUT 'localhost:9200/_river/places_suggest_river/_meta' -d "{
       \"type\" : \"jdbc\",
       \"jdbc\" : {
           \"driver\" : \"org.postgresql.Driver\",
